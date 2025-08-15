5025bfde6e1ce85aa21cce54dc142b6f15a5b5dd
Nadya <rarakirani08@gmail.com>
2025-08-15 08:27:48 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SeismoSens',
+  webDir: 'www'
+};
+
+export default config;
